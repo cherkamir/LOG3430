a44c3f506709f032e1e6de9a70f29c0ffa182b19
Mike Folepe <davyfolepe70@gmail.com>
2022-02-18 08:31:16 -0500

    Update TP2.py

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -59,17 +59,32 @@ def truth_table_dnf():
 # print(liste[len(liste)-1][3])
 liste = truth_table()
 amount_of_couples = 1
-print("Jeu de test pour le critere CACC:")
+# print("Jeu de test pour le critere CACC:")
+# for k in range(len(liste[0])-1):
+#     found = False
+#     for i in range(len(liste)):
+#         for j in range(len(liste)):    
+#             if (liste[i][k] != liste[j][k] and liste[i][4] != liste[j][4] and liste[i] != liste[j] and found != True):
+#                 char = ''
+#                 if k == 0: char = 'P'
+#                 if k == 1: char = 'H'
+#                 if k == 2: char = 'U'
+#                 if k == 3: char = 'G'
+#                 print(f"{amount_of_couples}. Pour la clause {char}, on a le couple {i} {liste[i]}, {j} {liste[j]}")
+#                 found = True
+#                 amount_of_couples += 1
+
+
+print("Jeu de test pour le critere GICC:")
 for k in range(len(liste[0])-1):
-    found = False
+    couple = False
     for i in range(len(liste)):
-        for j in range(len(liste)):    
-            if (liste[i][k] != liste[j][k] and liste[i][4] != liste[j][4] and liste[i] != liste[j] and found != True):
+            if ((liste[i][k] == True and liste[i][4] == True) or  (liste[i][k] == False and liste[i][4] == True) or (liste[i][k] == True and liste[i][4] == False) or (liste[i][k] == False and liste[i][4] == False) and couple != True):
                 char = ''
                 if k == 0: char = 'P'
                 if k == 1: char = 'H'
                 if k == 2: char = 'U'
                 if k == 3: char = 'G'
-                print(f"{amount_of_couples}. Pour la clause {char}, on a le couple {i} {liste[i]}, {j} {liste[j]}")
-                found = True
+ 
+                print(f"{amount_of_couples}. Pour la clause {char}, on a les tests {i} {liste[i]}")
                 amount_of_couples += 1
