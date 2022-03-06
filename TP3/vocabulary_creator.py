9bd9ea386ff2e491df8d56c860c4a22b86b4adc6
Mike Folepe <davyfolepe70@gmail.com>
2022-03-06 03:37:20 -0500

    Implementation conditon minimale d'occurence

--- TP3/vocabulary_creator.py
+++ TP3/vocabulary_creator.py
@@ -7,24 +7,36 @@ class VocabularyCreator:
     """Class for creating vocabulary of spam and non-spam messages"""
 
     def __init__(self):
-        self.train_set  = "train_set.json"
-        self.cleaning   = TextCleaning()
+        self.train_set = "train_set.json"
+        self.cleaning = TextCleaning()
         self.vocabulary = "vocabulary.json"
-        self.voc_data   = {}
-    
-    def compute_proba(self, data, total):
+        self.voc_data = {}
+
+    def compute_proba(self, data, total, min_occurrence):
         '''
         Description: calcule la probabilité de chaque mot du dictionnaire basé sur
         sa fréquence d'occurrence
         Sortie: le dictionnaire des probabilités pour chaque mot
         '''
         proba_dict = {}
+
+        # on cree une copie pour iterer et eliminer tous les mots qui n'ont pas le minimum de d'occurence requis
+        # On retire donc le mot et on diminue aussi le total d'occurences
+        temporary_data = data.copy()
+        if(min_occurrence < 5):
+            for wd in temporary_data:
+                nb_occ = temporary_data[wd]
+                if(nb_occ < min_occurrence):
+                    data.pop(wd)
+                    total -= nb_occ
+
+        # On effectue le reste du calcul apres avoir raccourci le data selon la condition minimale d'occurrence 
         for wd in data:
             proba_dict[wd] = data[wd] / total
 
         return proba_dict
 
-    def create_vocab(self, min_occurrence = 1,clean_option = 0):
+    def create_vocab(self, min_occurrence=1, clean_option=0):
         '''
         Description: fonction pour creer le vocabulaire des mots presents
         dans les e-mails spam et ham et le sauvegarder dans le fichier
@@ -37,13 +49,13 @@ class VocabularyCreator:
 
         occ_spam_sub = {}
         occ_spam_bod = {}
-        occ_ham_sub  = {}
-        occ_ham_bod  = {}
+        occ_ham_sub = {}
+        occ_ham_bod = {}
 
         total_occ_spam_sub = 0
-        total_occ_ham_sub  = 0
+        total_occ_ham_sub = 0
         total_occ_spam_bod = 0
-        total_occ_ham_bod  = 0
+        total_occ_ham_bod = 0
 
         email_count = len(dataset["dataset"])
         i = 0
@@ -52,19 +64,19 @@ class VocabularyCreator:
         for email in dataset["dataset"]:
             i += 1
             print("\rEmail " + str(i) + "/" + str(email_count), end="")
-            
+
             # Get data
-            data    = email["mail"]
+            data = email["mail"]
             subject = data["Subject"]
-            body    = data["Body"]
+            body = data["Body"]
             is_spam = False
 
-            # Update the number of spams / hams  
+            # Update the number of spams / hams
             if data["Spam"] == "true":
-                is_spam     = True
+                is_spam = True
 
-            # Analyze the subject 
-            subject = self.clean_text(subject,clean_option)
+            # Analyze the subject
+            subject = self.clean_text(subject, clean_option)
             if is_spam:
                 for wd in subject:
                     total_occ_spam_sub += 1
@@ -73,7 +85,7 @@ class VocabularyCreator:
                         occ_spam_sub[wd] = 1
                     else:
                         occ_spam_sub[wd] += 1
-            else: 
+            else:
                 for wd in subject:
                     total_occ_ham_sub += 1
                     # Add the word to the dictionary or update its occurence count
@@ -83,7 +95,7 @@ class VocabularyCreator:
                         occ_ham_sub[wd] += 1
 
             # Analyze the body
-            body = self.clean_text(body,clean_option)
+            body = self.clean_text(body, clean_option)
             if is_spam:
                 for wd in body:
                     total_occ_spam_bod += 1
@@ -92,7 +104,7 @@ class VocabularyCreator:
                         occ_spam_bod[wd] = 1
                     else:
                         occ_spam_bod[wd] += 1
-            else: 
+            else:
                 for wd in body:
                     total_occ_ham_bod += 1
                     # Add the word to the dictionary or update its occurence count
@@ -102,16 +114,16 @@ class VocabularyCreator:
                         occ_ham_bod[wd] += 1
 
         # Create the data dictionary
-        p_sub_spam  = self.compute_proba(occ_spam_sub, total_occ_spam_sub)
-        p_sub_ham   = self.compute_proba(occ_ham_sub, total_occ_ham_sub)
+        p_sub_spam = self.compute_proba(occ_spam_sub, total_occ_spam_sub)
+        p_sub_ham = self.compute_proba(occ_ham_sub, total_occ_ham_sub)
         p_body_spam = self.compute_proba(occ_spam_bod, total_occ_spam_bod)
-        p_body_ham  = self.compute_proba(occ_ham_bod, total_occ_ham_bod)
-        
+        p_body_ham = self.compute_proba(occ_ham_bod, total_occ_ham_bod)
+
         self.voc_data = {
-           "p_sub_spam": p_sub_spam,
-           "p_sub_ham": p_sub_ham,
-           "p_body_spam": p_body_spam,
-           "p_body_ham": p_body_ham
+            "p_sub_spam": p_sub_spam,
+            "p_sub_ham": p_sub_ham,
+            "p_body_spam": p_body_spam,
+            "p_body_ham": p_body_ham
         }
 
         # Save data
@@ -132,9 +144,9 @@ class VocabularyCreator:
             with open(self.vocabulary, "w") as outfile:
                 json.dump(vocab, outfile)
                 print("Vocab created")
-                return True 
+                return True
         except:
             return False
-    
+
     def clean_text(self, text, clean_option):
         return self.cleaning.clean_text(text, clean_option)
